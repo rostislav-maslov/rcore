23b7e48a8c3f1fe6106986fa9c26ad6da1ebb08a
ramaslov <maslov.rostislav@gmail.com>
2014-01-07 03:12:09 +0400

    upd - "Рыба" готовая к употреблению

--- src/main/java/com/ub/core/controllers/AdminTestController.java
+++ src/main/java/com/ub/core/controllers/AdminTestController.java
@@ -1,4 +1,15 @@
 package com.ub.core.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class AdminTestController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String printWelcome() {
+
+        return "com.ub.core.client.main";
+    }
 }
