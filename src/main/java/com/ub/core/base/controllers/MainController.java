31ce6cb08dad57e3ee446de656d5bb7495ff0c87
ramaslov <maslov.rostislav@gmail.com>
2014-01-15 23:34:38 +0400

    upd - новый шаблон

---
+++ src/main/java/com/ub/core/base/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.ub.core.base.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+        @RequestMapping(value = "/admin", method = RequestMethod.GET)
+        protected String add(Model model) throws Exception {
+            return "com.ub.core.admin.layout";
+        }
+
+}
--- src/main/webapp/static/a/0ecdc2a1f21af0053c8e1d78bc57a41a.js
+++
@@ -1,2 +0,0 @@
-!function(){$(function(){})}.call(this);
-;
