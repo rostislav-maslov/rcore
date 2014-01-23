1bf46d5d50bb65ba82d770c38a41fc879479b5e5
ramaslov <maslov.rostislav@gmail.com>
2014-01-24 00:22:04 +0400

    upd - меню

---
+++ src/main/java/com/ub/core/base/menu/ExcludeCoreMenu.java
@@ -0,0 +1,25 @@
+package com.ub.core.base.menu;
+
+import java.util.List;
+
+public abstract class ExcludeCoreMenu {
+
+    protected String id = getClass().getCanonicalName();
+    protected List<CoreMenu> menu;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<CoreMenu> getMenu() {
+        return menu;
+    }
+
+    public void setMenu(List<CoreMenu> menu) {
+        this.menu = menu;
+    }
+}
