55ae0ddd568e4d568cecb32794ca390c2176ab87
ramaslov <maslov.rostislav@gmail.com>
2014-04-23 15:35:02 +0400

    upd StringUtils.java

--- src/main/java/com/ub/core/base/utils/StringUtils.java
+++ src/main/java/com/ub/core/base/utils/StringUtils.java
@@ -10,4 +10,17 @@ public class StringUtils {
     public static boolean isNotEmpty(String s){
         return !isEmpty(s);
     }
+
+    public static String catString(String str, Integer l){
+        if(str == null) return null;
+        if(str.length() <= l) return str;
+
+        return str.substring(0, l-1);
+    }
+    public static String catStringWithDot(String str, Integer l){
+        if(str == null) return null;
+        if(str.length() <= l) return str;
+
+        return str.substring(0, l-4) + "...";
+    }
 }
