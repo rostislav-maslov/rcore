293a89c1092bbcfa21ea34c3a13f9dfbf349985e
ramaslov <maslov.rostislav@gmail.com>
2014-04-24 17:35:03 +0400

    mongo 1.4.2

--- src/main/java/com/ub/core/file/services/IFileService.java
+++
@@ -1,9 +0,0 @@
-package com.ub.core.file.services;
-
-import com.ub.core.file.store.FileInfo;
-import org.bson.types.ObjectId;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-
-public interface IFileService extends PagingAndSortingRepository<FileInfo, ObjectId> {
-}
