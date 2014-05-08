7d0f13e5e6957beb48e5bd4a3fdd7b8580ea97e2
ramaslov <maslov.rostislav@gmail.com>
2014-05-09 00:24:44 +0400

    auth

--- src/main/java/com/ub/core/autorization/controllers/AutorizationController.java
+++ src/main/java/com/ub/core/autorization/controllers/AutorizationController.java
@@ -1,7 +1,6 @@
 package com.ub.core.autorization.controllers;
 
 import com.ub.core.base.utils.RouteUtils;
-import com.ub.core.autorization.routes.AutorizationRoutes;
 import com.ub.core.security.service.AutorizationService;
 import com.ub.core.security.service.exceptions.UserNotAutorizedException;
 import com.ub.core.user.routes.UserLoginRoutes;
@@ -17,17 +16,7 @@ public class AutorizationController {
 
     @Autowired private AutorizationService autorizationService;
 
-    @RequestMapping(value = AutorizationRoutes.LOGIN, method = RequestMethod.POST)
-    public String autorization(@RequestParam String email, @RequestParam String password, Model model) {
-        try {
-            autorizationService.autorizeEmail(email, password);
-        } catch (UserNotAutorizedException e) {
-            model.addAttribute("title", "Ошибка при авторизации");
-            model.addAttribute("description", "Пользователя с таким email и паролем не существует.");
-            return "com.ub.dyl.client.registration.verification";
-        }
-        return RouteUtils.redirectTo("/");
-    }
+
 
     @RequestMapping(value = UserLoginRoutes.LOGIN, method = RequestMethod.GET)
     public String loginToAdmin(Model model) {
@@ -44,7 +33,7 @@ public class AutorizationController {
         return RouteUtils.redirectTo("/admin");
     }
 
-    @RequestMapping(value = AutorizationRoutes.LOGOUT, method = RequestMethod.GET)
+    @RequestMapping(value = UserLoginRoutes.LOGOUT, method = RequestMethod.GET)
     public String logout() {
         autorizationService.logout();
         return RouteUtils.redirectTo("/");
