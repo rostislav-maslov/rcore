1eeb160b55c3cbbd90cdb1a326cf8f4ffcd47fb8
Rostislav Maslov <maslov@unitbean.com>
2015-03-24 21:45:12 +0300

    PageHeader

---
+++ src/main/java/com/ub/core/base/views/pageHeader/PageHeader.java
@@ -0,0 +1,45 @@
+package com.ub.core.base.views.pageHeader;
+
+import com.ub.core.base.routes.BaseRoutes;
+import com.ub.core.base.views.breadcrumbs.Breadcrumbs;
+import com.ub.core.base.views.breadcrumbs.BreadcrumbsLink;
+
+
+public class PageHeader {
+    public static PageHeader defaultPageHeader(){
+        PageHeader pageHeader = new PageHeader();
+        Breadcrumbs breadcrumbs1 = new Breadcrumbs();
+        breadcrumbs1.getLinks().add(new BreadcrumbsLink(BaseRoutes.ADMIN,"Главная"));
+        pageHeader.setBreadcrumbs(breadcrumbs1);
+        pageHeader.setTitleAdd("Добавить");
+        return pageHeader;
+    }
+
+    private Breadcrumbs breadcrumbs = new Breadcrumbs();
+    private String titleAdd;
+    private String linkAdd;
+
+    public Breadcrumbs getBreadcrumbs() {
+        return breadcrumbs;
+    }
+
+    public void setBreadcrumbs(Breadcrumbs breadcrumbs) {
+        this.breadcrumbs = breadcrumbs;
+    }
+
+    public String getTitleAdd() {
+        return titleAdd;
+    }
+
+    public void setTitleAdd(String titleAdd) {
+        this.titleAdd = titleAdd;
+    }
+
+    public String getLinkAdd() {
+        return linkAdd;
+    }
+
+    public void setLinkAdd(String linkAdd) {
+        this.linkAdd = linkAdd;
+    }
+}
