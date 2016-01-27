6568499217f0c5a45ac8c141866c45caebd7382a
Наздрачёв Михаил <mpnazdrachyov@unitbean.com>
2016-01-27 21:11:15 +0300

    Добавил авторизацию через linkedin

--- src/main/java/com/ub/core/security/session/SessionModel.java
+++ src/main/java/com/ub/core/security/session/SessionModel.java
@@ -32,6 +32,8 @@ public class SessionModel {
             httpSession.setAttribute(TYPE, "O");
         if (type.equals(SessionType.TWITTER))
             httpSession.setAttribute(TYPE, "T");
+        if (type.equals(SessionType.LINKEDIN))
+            httpSession.setAttribute(TYPE, "L");
 
         return httpSession;
     }
@@ -55,6 +57,8 @@ public class SessionModel {
                 type = SessionType.OK;
             if (t.equals("T"))
                 type = SessionType.TWITTER;
+            if (t.equals("L"))
+                type = SessionType.LINKEDIN;
         }
     }
 
