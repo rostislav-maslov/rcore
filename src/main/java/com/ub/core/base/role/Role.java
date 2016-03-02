52dd17f5731cdf7c6ad6ddde0d02d134407df7d3
edkornev <edkornev@gmail.com>
2016-03-02 15:14:33 +0300

    Fix: equals for Role

--- src/main/java/com/ub/core/base/role/Role.java
+++ src/main/java/com/ub/core/base/role/Role.java
@@ -28,7 +28,7 @@ public class Role {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || !(o instanceof Role)) return false;
 
         Role that = (Role) o;
 
