c2917ef35fb6758a8cc21fbe1e5e5fb36d0d6f8b
Rostislav Maslov <maslov.rostislav@gmail.com>
2020-04-08 07:08:32 +0400

    Change CRUDRepository interface

--- domain/src/main/java/com/rcore/domain/base/entity/BaseEntity.java
+++ domain/src/main/java/com/rcore/domain/base/entity/BaseEntity.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.util.Date;
 
 @Data
-public class BaseEntity {
+public abstract class BaseEntity {
     protected Date createdAt = new Date();
     protected Date updatedAt  = new Date();
     protected String timeZone = "GMT";
--- domain/src/main/java/com/rcore/domain/base/port/CRUDRepository.java
+++ domain/src/main/java/com/rcore/domain/base/port/CRUDRepository.java
@@ -1,14 +1,31 @@
 package com.rcore.domain.base.port;
 
+import com.rcore.domain.base.entity.BaseEntity;
+
+import java.util.Date;
 import java.util.Optional;
 
-public interface CRUDRepository<IdT,ObjectT> {
+public abstract class CRUDRepository<IdT, ObjectT extends BaseEntity> {
 
-    Optional<ObjectT> save(ObjectT object);
-    Boolean delete(ObjectT object);
-    Boolean deleteById(IdT id);
-    Optional<ObjectT> findById(IdT id);
-    SearchResult<ObjectT> find(Long size, Long skip);
-    Long count();
+    protected abstract Optional<ObjectT> saveToRepository(ObjectT object);
+
+    public abstract Boolean delete(ObjectT object);
+
+    public abstract Boolean deleteById(IdT id);
+
+    public abstract Optional<ObjectT> findById(IdT id);
+
+    public abstract SearchResult<ObjectT> find(Long size, Long skip);
+
+    public abstract Long count();
+
+    public Optional<ObjectT> save(ObjectT object) {
+        if (object.getCreatedAt() == null) {
+            object.setCreatedAt(new Date());
+        }
+        object.setUpdatedAt(new Date());
+
+        return saveToRepository(object);
+    }
 
 }
