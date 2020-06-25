ce8e4167ecc2db3146accdfbe2ea37c34ed2c3b6
Владимир Козлов <kozlovva@sushivesla.team>
2020-06-25 09:52:00 +0400

    Added custom LocalDateTimeSerializer

--- rest-api/src/main/java/com/rcore/restapi/config/CustomObjectMapperBuilder.java
+++ rest-api/src/main/java/com/rcore/restapi/config/CustomObjectMapperBuilder.java
@@ -3,6 +3,7 @@ package com.rcore.restapi.config;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.rcore.restapi.config.jackson.LocalDateTimeDeserializer;
+import com.rcore.restapi.config.jackson.LocalDateTimeSerializer;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +14,7 @@ public class CustomObjectMapperBuilder {
 
     public Jackson2ObjectMapperBuilder getJackson2ObjectMapperBuilder() {
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-        builder.serializerByType(LocalDateTime.class, new ToStringSerializer());
+        builder.serializerByType(LocalDateTime.class, new LocalDateTimeSerializer());
         builder.serializationInclusion(JsonInclude.Include.NON_NULL);
         builder.deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer());
         return builder;
