9c11bb8ac50613488f1d7eb7be871dcbd949d8e0
Владимир Козлов <kozlovva@sushivesla.team>
2020-07-07 11:57:15 +0400

    Fix LocalDateSerializer - dateTime to UTC

--- rest-api/src/main/java/com/rcore/restapi/config/jackson/LocalDateTimeSerializer.java
+++ rest-api/src/main/java/com/rcore/restapi/config/jackson/LocalDateTimeSerializer.java
@@ -6,12 +6,13 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
 
     @Override
     public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        gen.writeString(DateTimeFormatter.ISO_DATE_TIME.format(value));
+        gen.writeString(DateTimeFormatter.ISO_DATE_TIME.format(value.atZone(ZoneId.of("UTC"))));
     }
 }
