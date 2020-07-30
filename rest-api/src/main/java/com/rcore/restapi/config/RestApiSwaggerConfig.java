d67096062317b68b7d914e2644ab36b10f613229
Владимир Козлов <kozlovva@sushivesla.team>
2020-07-30 15:48:36 +0400

    Swagger - init bean only for dev profile

--- rest-api/src/main/java/com/rcore/restapi/config/RestApiSwaggerConfig.java
+++ rest-api/src/main/java/com/rcore/restapi/config/RestApiSwaggerConfig.java
@@ -2,6 +2,7 @@ package com.rcore.restapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @Configuration
+@Profile("dev")
 public class RestApiSwaggerConfig {
 
     @Bean
