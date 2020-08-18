fa2463337812b7757814cd064ae2ed2e5aba19fe
Владимир Козлов <kozlovva@sushivesla.team>
2020-08-18 11:40:34 +0400

    Get IP from request

--- rest-api/src/main/java/com/rcore/restapi/utils/WebRequestUtils.java
+++ rest-api/src/main/java/com/rcore/restapi/utils/WebRequestUtils.java
@@ -1,9 +1,14 @@
 package com.rcore.restapi.utils;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.rcore.restapi.headers.WebHeaders.*;
 
@@ -40,8 +45,30 @@ public class WebRequestUtils {
     public static String getLanguage() {
         return getHeader(X_LANGUAGE);
     }
-    //TODO реализовать
+
     public static String getIp() {
-        return "ip";
+        final String[] ip = new String[1];
+        //IP candidates
+        Arrays.asList("X-Forwarded-For",
+                "Proxy-Client-IP",
+                "WL-Proxy-Client-IP",
+                "HTTP_X_FORWARDED_FOR",
+                "HTTP_X_FORWARDED",
+                "HTTP_X_CLUSTER_CLIENT_IP",
+                "HTTP_CLIENT_IP",
+                "HTTP_FORWARDED_FOR",
+                "HTTP_FORWARDED",
+                "HTTP_VIA",
+                "REMOTE_ADDR")
+                .forEach(header -> {
+                    String ipFromHeader = getHeader(header);
+                    if (StringUtils.hasText(ipFromHeader)) {
+                        ip[0] = ipFromHeader.split(",")[0];
+                        return;
+                    }
+
+                });
+
+        return StringUtils.hasText(ip[0]) ? ip[0] : getRequest().getRemoteAddr();
     }
 }
