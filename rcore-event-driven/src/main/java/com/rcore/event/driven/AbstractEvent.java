df99c48b38c63809564605c1108184a1486ddb51
Владимир Козлов <kozlovva@sushivesla.team>
2021-03-04 13:46:34 +0300

    Fix eventDispatcher

--- rcore-event-driven/src/main/java/com/rcore/event/driven/AbstractEvent.java
+++ rcore-event-driven/src/main/java/com/rcore/event/driven/AbstractEvent.java
@@ -1,14 +1,28 @@
 package com.rcore.event.driven;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
 public abstract class AbstractEvent implements Event {
 
     protected String id = UUID.randomUUID().toString();
     protected String traceId = UUID.randomUUID().toString();
+    protected LocalDateTime date = LocalDateTime.now();
 
     public AbstractEvent(String traceId) {
         this.traceId = traceId;
+    }
+
+    public AbstractEvent(String traceId, LocalDateTime date) {
+        this.traceId = traceId;
+        this.date = date;
+    }
+
+    public AbstractEvent(LocalDateTime date) {
+        this.date = date;
     }
 
     public AbstractEvent() {
--- rcore-event-driven/src/main/java/com/rcore/event/driven/EventDispatcher.java
+++ rcore-event-driven/src/main/java/com/rcore/event/driven/EventDispatcher.java
@@ -11,8 +11,8 @@ public abstract class EventDispatcher {
 
     private EventStorage eventStorage;
 
-    abstract <E extends Event> void registerHandler(Class<E> eventType, EventHandler<E> handler);
+    public abstract <E extends Event> void registerHandler(Class<E> eventType, EventHandler<E> handler);
 
-     abstract <E extends Event> void dispatch(E event);
+    public abstract <E extends Event> void dispatch(E event);
 
 }
