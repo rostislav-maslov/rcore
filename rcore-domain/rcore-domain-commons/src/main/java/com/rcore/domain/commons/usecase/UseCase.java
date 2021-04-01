9ce54b75ada0c7204b7c4d28f84039ebc1aeef3c
Владимир Козлов <kozlovva@sushivesla.team>
2021-04-01 13:36:25 +0300

    Добавлен валидатор

--- rcore-domain/rcore-domain-commons/src/main/java/com/rcore/domain/commons/usecase/UseCase.java
+++ rcore-domain/rcore-domain-commons/src/main/java/com/rcore/domain/commons/usecase/UseCase.java
@@ -1,11 +1,16 @@
 package com.rcore.domain.commons.usecase;
 
+import javax.validation.Validation;
+import javax.validation.Validator;
+
 /**
  * UseCase - это определенный тип. Он содержит в себе информацию о модели входящих и исходящих данных. Он имеет один метод - выполнить useCase
  * @param <Input>
  * @param <Output>
  */
 public abstract class UseCase<Input extends UseCase.InputValues, Output extends UseCase.OutputValues> {
+
+    protected final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
     /**
      * Выполнение useCase
