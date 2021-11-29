62e8cb0e0905716d571a6734bc1b467cd9136371
Spliterash <me@spliterash.ru>
2021-11-29 16:48:30 +0300

    FEAT-864: Backend: для Rcore в классе AbstractEvent добавить аннотацию SuperBuilder

--- rcore-event-driven/src/main/java/com/rcore/event/driven/AbstractEvent.java
+++ rcore-event-driven/src/main/java/com/rcore/event/driven/AbstractEvent.java
@@ -1,15 +1,20 @@
 package com.rcore.event.driven;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Getter
+@SuperBuilder
 public abstract class AbstractEvent implements Event {
-
+    @Builder.Default
     protected String id = UUID.randomUUID().toString();
+    @Builder.Default
     protected String traceId = UUID.randomUUID().toString();
+    @Builder.Default
     protected Instant date = Instant.now();
 
     public AbstractEvent(String traceId) {
