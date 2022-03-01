bdc1b33dc4de99a9795eb58b3c647f6959a26071
Владимир Козлов <kozlovva@sushivesla.team>
2022-03-01 16:37:06 +0300

    AccessChecker - добавлен метод для валидации токена

--- rcore-rest-api/rcore-rest-api-spring-security/src/main/java/com/rcore/rest/api/spring/security/DefaultAccessChecker.java
+++ rcore-rest-api/rcore-rest-api-spring-security/src/main/java/com/rcore/rest/api/spring/security/DefaultAccessChecker.java
@@ -1,17 +1,30 @@
 package com.rcore.rest.api.spring.security;
 
+import com.rcore.domain.security.model.AccessTokenData;
 import com.rcore.domain.security.model.CredentialDetails;
 import com.rcore.domain.security.port.AccessChecker;
 import com.rcore.domain.security.port.CredentialIdentityService;
+import com.rcore.domain.security.port.TokenParser;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class DefaultAccessChecker implements AccessChecker {
 
     private final CredentialIdentityService credentialIdentityService;
+    private final TokenParser<AccessTokenData> accessTokenDataTokenParser;
 
     @Override
     public CredentialDetails checkAccessByToken(String token, String requestType, String requestPath, String serviceName) {
         return credentialIdentityService.getCredentialByToken(token);
+    }
+
+    @Override
+    public boolean tokenIsValid(String token) {
+        try {
+            accessTokenDataTokenParser.parseWithValidating(token);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 }
