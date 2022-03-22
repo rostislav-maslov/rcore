5d8c1b06c365b188c22618908696aa78bc6f146c
Владимир Козлов <kozlovva@sushivesla.team>
2022-03-22 15:05:06 +0300

    добавлен паттерн для валидации email

---
+++ rcore-domain/rcore-domain-commons/src/main/java/com/rcore/domain/commons/validators/ValidationPatterns.java
@@ -0,0 +1,5 @@
+package com.rcore.domain.commons.validators;
+
+public class ValidationPatterns {
+    public static final String EMAIL_PATTERN = "[a-zA-Z\\d]{1}[a-zA-Z-._+'\\d]{0,62}[a-zA-Z\\d]{1}[@]{1}[a-zA-Z\\d]{1}[a-zA-Z-.\\d]{0,62}[a-zA-Z\\d]{1}[.][a-zA-Z]{2,64}";
+}
